d8f023b5ffd061e56bd1153e356a1dff4aba250c
AwdotiaRomanowna <zaledwie10minut@gmail.com>
2020-09-23 01:00:18 +0200

    adjuting to libpq standard variables

--- bench.sh
+++ bench.sh
@@ -6,35 +6,33 @@ echo "Welcome to the pgbench auto-config"
 
 echo "Please provide your database host (default localhost)"
 
-read dbhost
+read PGHOST
 
-if test -z "$dbhost"
+if test -z "$PGHOST"
 then
-	export DBHOST=localhost
-else
-	export DBHOST=$dbhost
+    export PGHOST=$PGHOST
 fi
 
 echo "Please provide your postgres username"
 
-read dbuser
+read PGUSER
 
-if test -z "$dbuser"
+if test -z "$PGUSER"
 then
-	echo "you must provide a valid username"
+        echo "you must provide a valid username"
 else
-	export DBUSER=$dbuser
+        export PGUSER=$PGUSER
 fi
 
 echo "Please provide your database name (default postgres)"
 
-read postgresdb
+read PGDATABASE
 
-if test -z "$postgresdb"
+if test -z "$PGDATABASE"
 then
-	export POSTGRESDBNAME=postgres
+        export PGDATABASE=postgres
 else
-	export POSTGRESDBNAME=$postgresdb
+        export PGDATABASE=$PGDATABASE
 fi
 
 
@@ -44,9 +42,9 @@ read workerthreads
 
 if test -z "$workerthreads"
 then
-	export PGTHREADS=1
+        export PGTHREADS=1
 else
-	export PGTHREADS=$workerthreads
+        export PGTHREADS=$workerthreads
 fi
 echo "Please provide a number of simulated clients (Should be a multitude of threads. Default 1)"
 
@@ -54,9 +52,9 @@ read pgclients
 
 if test -z "$pgclients"
 then
-	export PGCLIENTS=1
+        export PGCLIENTS=1
 else
-	export PGCLIENTS=$pgclients
+        export PGCLIENTS=$pgclients
 fi
 echo "Please provide a number of transactions each client runs (default 10)"
 
@@ -64,24 +62,24 @@ read transactions
 
 if test -z "$transactions"
 then
-	export PGTRANS=10
+        export PGTRANS=10
 else
-	export PGTRANS=$transactions
+        export PGTRANS=$transactions
 fi
 
 
 echo summary:
 
-echo "database host: $DBHOST, database user: $DBUSER, database name: $POSTGRESDBNAME, worker threads: $PGTHREADS, simulated clients: $PGCLIENTS, transactions per client: $PGTRANS"
+echo "database host: $PGHOST, database user: $PGUSER, database name: $PGDATABASE, worker threads: $PGTHREADS, simulated clients: $PGCLIENTS, transactions per client: $PGTRANS"
 
 read -p "Did you already initialized pgbench (pgbench -i)?  <y/N> " prompt
 if [[ $prompt =~ [yY](es)* ]]
 then
-	echo ""
+        echo ""
 else 
-	echo "Initializing pgbench with command: pgbench -i -h $DBHOST -U $DBUSER $POSTGRESDBNAME"
-	pgbench -i -h $DBHOST -U $DBUSER $POSTGRESDBNAME 
-	
+        echo "Initializing pgbench with command: pgbench -i -h $PGHOST -U $PGUSER $PGDATABASE"
+        pgbench -i 
+
 fi
 
 #TODO set vars as "global" env vars.
@@ -89,18 +87,18 @@ fi
 read -p "Continue with benchmark?  <y/N> " prompt
 if [[ $prompt =~ [yY](es)* ]]
 then
-	echo "running command: pgbench -h $DBHOST -j$PGTHREADS -r -Mextended -n -c$PGCLIENTS -t$PGTRANS -U $DBUSER $POSTGRESDBNAME >> ./pgbench_results
+        echo "running command: pgbench -h $PGHOST -j$PGTHREADS -r -Mextended -n -c$PGCLIENTS -t$PGTRANS -U $PGUSER $PGDATABASE >> ./pgbench_results
  ... "
-	
-	echo "Benchmark-test time: $(date) against host: $DBHOST" >> ./pgbench_results
-	start=$SECONDS
-	pgbench -h $DBHOST -j$PGTHREADS -r -Mextended -n -c$PGCLIENTS -t$PGTRANS -U $DBUSER $POSTGRESDBNAME >> ./pgbench_results
-	end=$SECONDS
-	let diff=end-start
-	echo "Output concatenated to file ./pgbench_results"
-	echo "duration of execution: $diff seconds" >> ./pgbench_results
+
+        echo "Benchmark-test time: $(date) against host: $PGHOST" >> ./pgbench_results
+        start=$SECONDS
+        pgbench -j$PGTHREADS -r -Mextended -n -c$PGCLIENTS -t$PGTRANS >> ./pgbench_results
+        end=$SECONDS
+        let diff=end-start
+        echo "Output concatenated to file ./pgbench_results"
+        echo "duration of execution: $diff seconds" >> ./pgbench_results
 else 
-	echo "cancel benchmark..."
+        echo "cancel benchmark..."
 fi
 
 exit 0
